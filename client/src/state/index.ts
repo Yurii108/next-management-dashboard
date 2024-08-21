5b5afcbf2d534c55699795ca54c4b4d8c658d734
Yurii Paraska <paraska108@gmail.com>
2024-08-21 18:52:39 +0300

    feat: add redux store with redux-persist

---
+++ client/src/state/index.ts
@@ -0,0 +1,28 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+export interface InitialStateType {
+  isSidebarCollapsed: boolean;
+  isDarkMode: boolean;
+}
+
+const initialState: InitialStateType = {
+  isSidebarCollapsed: false,
+  isDarkMode: false,
+};
+
+export const globalSlice = createSlice({
+  name: "global",
+  initialState,
+  reducers: {
+    setSidebarCollapsed: (state, action: PayloadAction<boolean>) => {
+      state.isSidebarCollapsed = action.payload;
+    },
+    setIsDarkMode: (state, action: PayloadAction<boolean>) => {
+      state.isDarkMode = action.payload;
+    },
+  },
+});
+
+export const { setSidebarCollapsed, setIsDarkMode } = globalSlice.actions;
+
+export default globalSlice.reducer;
