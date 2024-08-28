1f046758b4def8da86bd383d76e58926f8c268e9
Yurii Paraska <paraska108@gmail.com>
2024-08-28 19:32:18 +0300

    feat:add controller

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,14 +8,19 @@ import bodyParser from 'body-parser';
 dotenv.config();
 const app = express()
 app.use(express.json())
-app.use(helmet)
+app.use(helmet())
 app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }))
 app.use(morgan('common'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cors())
 
+/* ROUTES */
+app.get("/hello", (req, res) => {
+    res.send("hello world");
+})
 
+/* SERVER */
 const port = process.env.PORT || 3001
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
