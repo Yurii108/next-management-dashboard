5c07499dd4c4a57f53489c1bd6d69e9c532065fb
Yurii Paraska <paraska108@gmail.com>
2024-08-28 18:46:39 +0300

    feat: add index.ts server and run script

---
+++ server/src/index.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import bodyParser from 'body-parser';
+
+dotenv.config();
+const app = express()
+app.use(express.json())
+app.use(helmet)
+app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }))
+app.use(morgan('common'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cors())
+
+
+const port = process.env.PORT || 3001
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+})
