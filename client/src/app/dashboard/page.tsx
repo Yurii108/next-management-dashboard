ead14391c2e2a09ee08085129cd838794c616c67
Yurii Paraska <paraska108@gmail.com>
2024-08-28 20:59:09 +0300

    feat: start creat dashboard page

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -1,9 +1,15 @@
+"use client";
+
 import React from "react";
 
 type Props = {};
 
 const Dashboard = (props: Props) => {
-  return <div>Dashboard</div>;
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:overflow-auto gap-10 pb-4 custom-grid-rows">
+      <div className="row-span-3 xl:row-span-6 bg-gray-500" />
+    </div>
+  );
 };
 
 export default Dashboard;
