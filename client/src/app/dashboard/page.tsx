bae47a9b8cffcc35b5103f90a4edf7a523666544
Yurii Paraska <paraska108@gmail.com>
2024-08-31 11:32:31 +0300

    feat: add CardPurchaseSummary

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -3,6 +3,7 @@
 import React from "react";
 import CartPopularProducts from "./CartPopularProducts";
 import CardSalesSummary from "./CardSalesSummary";
+import CardPurchaseSummary from "./CardPurchaseSummary";
 
 type Props = {};
 
@@ -11,7 +12,7 @@ const Dashboard = (props: Props) => {
     <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:overflow-auto gap-10 pb-4 custom-grid-rows">
       <CartPopularProducts />
       <CardSalesSummary />
-      <div className="row-span-2 xl:row-span-3 col-span-1 md:col-span-2 xl:col-span-1 bg-gray-500" />
+      <CardPurchaseSummary />
       <div className="row-span-3 bg-gray-500" />
       <div className="row-span-1 xl:row-span-2 bg-gray-500" />
       <div className="row-span-1 xl:row-span-2 bg-gray-500" />
